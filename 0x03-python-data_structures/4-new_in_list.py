1dadd5195ead4aee504db394cd2d102c8ae544b7
tinsae-tadese <newmee1523@gmail.com>
2022-08-30 08:20:15 -0700

    Python Data Structure

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     newllist = my_list.copy()
     for items in newllist:
         if(idx < 0):
             return newllist
-        elif(idx >= len(newllist)):
+        elif(idx > len(newllist) - 1):
             return newllist
         else:
             newllist[idx] = element
