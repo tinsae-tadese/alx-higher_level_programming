548b9945feedf2c8d34fa162820166e968d7400b
tinsae-tadese <newmee1523@gmail.com>
2022-11-21 05:15:58 -0800

    1-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Creates a class Square
+"""
+
+
+class Square:
+    """ Square Class
+    """
+
+    def __init__(self, size=0):
+        """
+        Constructor with instantiation
+        of size
+        """
+        if type(size) is not int:
+            raise TypeError("Size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
