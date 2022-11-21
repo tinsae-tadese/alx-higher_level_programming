321ffb161428e05d7fdfd6e448984b52cb916b5b
tinsae-tadese <newmee1523@gmail.com>
2022-11-21 05:00:20 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python3
+""" Creates a class Square """
+
+
 class Square:
-    """square class """
-    
+    """ Square Class
+    """
+
     def __init__(self, size):
         self.size = size
 
