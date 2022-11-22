822c314c94f0e0d7f32bd86b07a5d72d0caec860
tinsae-tadese <newmee1523@gmail.com>
2022-11-21 22:01:59 -0800

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,7 +13,7 @@ class Square:
         of size
         """
         if type(size) is not int:
-            raise TypeError("Size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
