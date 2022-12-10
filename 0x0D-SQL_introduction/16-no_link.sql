fc16ce795cdecd7635e7826a05977527fb62fe42
tinsae-tadese <newmee1523@gmail.com>
2022-12-10 03:55:52 -0800

    Listing records where name is not null

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- writing a script that lists all records of the table 
 -- database name will ba passed as an argument 
-SELECT score,name FROM second_table ORDER BY score DESC;
+SELECT score,name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
