4ead83ef50c61a25ba63e4a0a08cd7f0edaab2fc
tinsae-tadese <newmee1523@gmail.com>
2022-12-10 03:17:05 -0800

    Removing records with score value <= 5

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,3 @@
 -- removing all records with score less than 5.
 -- database name will be passed as an argument.
-DELETE * FROM second_table WHERE scorre <=5;
+DELETE FROM second_table WHERE scorre <= 5;
