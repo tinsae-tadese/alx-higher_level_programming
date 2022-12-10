cb69a20d96d18071c0d57e7f71323d1a0e134371
tinsae-tadese <newmee1523@gmail.com>
2022-12-09 23:52:22 -0800

    selecting row with id = 89

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- displaying the numbers of records with id = 89
 -- database name will be passed as an argument
-SELECT * FROM first_table WHERE id = 89;
+SELECT count(*) FROM first_table WHERE id = 89;
