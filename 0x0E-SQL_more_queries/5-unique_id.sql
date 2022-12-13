43abaae030fc42cfe36a3451dcd8f987aaa91965
tinsae-tadese <newmee1523@gmail.com>
2022-12-13 00:50:46 -0800

    Creating table whith unique attributes

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Creating table unique attributes
-CREATE TABLE IF NOT EXISTS unique_id.sql(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
