cf94db632234f10a00afc4aa6b646dcaaa719ff3
tinsae-tadese <newmee1523@gmail.com>
2022-12-13 05:20:43 -0800

    Fetching data using sub queries

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Script that lists all states of californi
 USE hbtn_0d_usa;
-SELECT id,name FROM cities WHERE id = SELECT id FROM states WHERE name = 'California');
+SELECT id,name FROM cities WHERE state_id in (SELECT id FROM states WHERE name = 'California'));
