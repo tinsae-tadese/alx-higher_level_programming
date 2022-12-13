79390a88c89ece8d6fc53c4300034554c8465113
tinsae-tadese <newmee1523@gmail.com>
2022-12-13 06:12:36 -0800

    Fetching data using joins | inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Sscript that lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name,states.name FROM cities inner join states on cities.stated_id = states.id ORDER BY cites.id ASC;
+SELECT cities.id, cities.name,states.name FROM cities inner join states on cities.states_id = states.id ORDER BY cites.id ASC;
